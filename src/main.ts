a22f78bffc9d723f309f79de8bd92cc9c386f6f1
Marco Muser <marco.muser@enpal.de>
2023-09-30 10:10:22 +0200

    error openapi-fetch in commonjs file

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import createClient from "openapi-fetch";
+
 export const main = () => {
-    return "hello world"
+    return `'createClient' is declared but its value is never read.ts(6133)
+    The current file is a CommonJS module whose imports will produce 'require' calls; however, the referenced file is an ECMAScript module and cannot be imported with 'require'. Consider writing a dynamic 'import("openapi-fetch")' call instead.
+    To convert this file to an ECMAScript module, change its file extension to '.mts', or add the field "type": "module" to '/Users/marco/Documents/repos/cjsts/package.json'.ts(1479)`
 }
